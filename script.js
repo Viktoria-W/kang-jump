045180e0572be82fe5e95d1d738135ad5e811056
Victoria <vicktorianedopekina@yandex.ru>
2022-04-13 20:41:36 +0400

    touchstart added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cactus = document.getElementById('cactus');
 document.addEventListener("keydown", function(event) {
     jump()
 });
-document.addEventListener("toustart", function(event) {
+document.addEventListener("touchstart", function(event) {
     jump()
 });
 //Добавление класса jump к кенгуру
