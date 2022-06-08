fe404f2e1f92274ed258bcb219f2d7c0de009ef0
Victoria <vicktorianedopekina@yandex.ru>
2022-06-08 17:52:50 +0400

    change

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
 const kangaroo = document.getElementById('kangaroo');
 const cactus = document.getElementById('cactus');
+const button = document.querySelector('button');
+let counter = document.getElementById('counter');
+let counterValue = 0;
 
-document.addEventListener("keydown", function(event) {
-    jump()
-});
-document.addEventListener("touchend", function(event) {
-    jump()
-});
+//начало работы от кнопки старт
+button.addEventListener('click', () => {
+    document.addEventListener('keydown', () => {
+        jump()
+        count()
+    });
+
+    document.addEventListener('touchend', () => {
+        jump()
+        count()
+    });
+
+    button.setAttribute('hidden', 'hidden');
+    cactus.style.animation = 'cactusMov 1.5s infinite linear';
+})
+
+//очки игры
+function count() {
+    counterValue += 1;
+    counter.innerHTML = counterValue;
+}
+
+
+
 //Добавление класса jump к кенгуру
-function jump () {
-    if(kangaroo.classList != "jump") {
+function jump() {
+    if (kangaroo.classList != "jump") {
         kangaroo.classList.add('jump');
     }
-    setTimeout( function() {
+
+    setTimeout(function () {
         kangaroo.classList.remove('jump');
     }, 400)
 };
+
+
 //получение высоты и кенгуру и кактуса + выведение ошибки при их соприкосновении
-let isAlive = setInterval ( function () {
+let isAlive = setInterval(function () {
     let kangarooTop = parseInt(window.getComputedStyle(kangaroo).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    if(cactusLeft < 50 && cactusLeft > 0 && kangarooTop >= 240) {
-        alert('GAME OVER!!!');
+    if (cactusLeft < 40 && cactusLeft > 0 && kangarooTop >= 240) {
+        alert(`GAME OVER!!! \nТы набрал ${counter.textContent} очка(-ов)`);
+
+        button.removeAttribute('hidden');
+
+        cactus.style.animation = '';
+
+        window.location.reload();
     }
+
 }, 10);
